0ed54534fdca85e424b5445590882575aadaf4f0
MartinPaGarcia <a01066569@itesm.mx>
2021-11-01 13:52:08 -0600

    Separacion de boton en nuevo widget y creacion de dos botones adicionales

--- web_prueba/lib/src/Pages/contador_page.dart
+++ web_prueba/lib/src/Pages/contador_page.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Contador extends StatefulWidget {
-  const Contador({ Key? key }) : super(key: key);
+  const Contador({Key? key}) : super(key: key);
 
   @override
   _ContadorState createState() => _ContadorState();
@@ -14,27 +16,37 @@ class _ContadorState extends State<Contador> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text("Stateful"),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Numero de clicks:", style: _estiloTexto),
-          Text("$_contador", style: _estiloTexto)
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _contador ++;
-          });
-        },
-      ),
+        appBar: AppBar(
+          // ignore: prefer_const_constructors
+          title: Text("Stateful"),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Numero de clicks:", style: _estiloTexto),
+            Text("$_contador", style: _estiloTexto)
+          ],
+        )),
+        floatingActionButton: _crearBotones());
+  }
+
+  // ignore: duplicate_ignore
+  Widget _crearBotones() {
+    // ignore: prefer_const_constructors
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: null),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: null),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: null),
+      ],
     );
+
+    
   }
 }
