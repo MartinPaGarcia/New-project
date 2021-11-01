6a8b392cd3bc2dc761e90382a10fb809ebb6f960
MartinPaGarcia <a01066569@itesm.mx>
2021-11-01 13:20:59 -0600

    Se añadio la variable contador a la pantalla

--- web_prueba/lib/src/Pages/home_page.dart
+++ web_prueba/lib/src/Pages/home_page.dart
@@ -1,38 +1,35 @@
-
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-
-  final estiloTexto =  const TextStyle(fontSize: 25);
+  final estiloTexto = const TextStyle(fontSize: 25);
+  final contador = 0;
 
   const HomePage({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // ignore: prefer_const_constructors
-          title: Text("Titulo"),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:  <Widget>[
-            Text("Numero de clicks:", style:estiloTexto),
-            Text("0", style:estiloTexto )
-          ],
-        )
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon( Icons.add ),
-          onPressed: (){
-            // ignore: avoid_print
-            print("Hola mundo");
-          },
-          ),
-        );
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        title: Text("Titulo"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Numero de clicks:", style: estiloTexto),
+          Text("$contador", style: estiloTexto)
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // ignore: avoid_print
+          print("Hola mundo");
+          //contador += 1;
+        },
+      ),
+    );
   }
 }
