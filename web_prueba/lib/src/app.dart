5bd835b3d0a28508715930f92664dfcef906fdff
MartinPaGarcia <a01066569@itesm.mx>
2021-11-01 13:20:12 -0600

    Cambio del home a contador

--- web_prueba/lib/src/app.dart
+++ web_prueba/lib/src/app.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-import 'Pages/home_page.dart';
+import 'package:web_prueba/src/Pages/contador_page.dart';
+//import 'Pages/home_page.dart';
+import 'Pages/contador_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Center(
-        child: HomePage(),
+        //child: HomePage(),
+        child: Contador(),
       ),
     );
   }
