8b81e0b8e7fc5a315f845f3771a9a10a5d6e03e2
MartinPaGarcia <a01066569@itesm.mx>
2021-11-01 12:37:19 -0600

    Estilo definido en Hompage.dart

--- web_prueba/lib/src/Pages/home_page.dart
+++ web_prueba/lib/src/Pages/home_page.dart
@@ -1,19 +1,29 @@
+
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+
+  final estiloTexto =  const TextStyle(fontSize: 25);
+
+  const HomePage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text("Titulo"),
-        centerTitle: true,       
-      ),
-       body: const Center(
-          child: Text("Tiembla JP"),
-         )
-    );
+        appBar: AppBar(
+          // ignore: prefer_const_constructors
+          title: Text("Titulo"),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  <Widget>[
+            Text("Numero de clicks:", style:estiloTexto),
+            Text("0", style:estiloTexto )
+          ],
+        )));
   }
 }
